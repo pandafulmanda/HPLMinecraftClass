33b3d4a0d0cebc83e07e0ff22749f5e1188125c5
Neeraj Tandon <neerajtandon25@gmail.com>
2014-06-11 18:59:56 -0400

    setting things up

---
+++ HPLMinecraftClass/HPLMinecraftClass.js
@@ -0,0 +1,35 @@
+Participants = new Meteor.Collection("participants");
+
+if (Meteor.isClient) {
+
+Template.foo.events({'submit form' : function(event, template) {
+    event.preventDefault();
+
+    firstname = template.find("input[name=firstname]");
+    lastname = template.find("input[name=lastname]");   
+    email = template.find("input[name=email]");
+
+    // Do form validation
+
+    var data = {
+      firstname: firstname.value,
+      lastname: lastname.value,
+      email: email.value
+    };
+
+    email.value="";
+    firstname.value="";
+    lastname.value="";
+
+    Participants.insert(data);//, function(err) { /* handle error */ });
+    console.log("count is up to " + Participants.find().count())
+
+}});
+
+  };
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}
