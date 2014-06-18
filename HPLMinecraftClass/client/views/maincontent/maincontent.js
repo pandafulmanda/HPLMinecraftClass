030debf26d06f4e295d89a755078b9a52c0ee8ac
neerajt <neerajtandon25@gmail.com>
2014-06-17 23:34:34 -0500

    adding reactivity to main section

--- HPLMinecraftClass/client/views/maincontent/maincontent.js
+++ HPLMinecraftClass/client/views/maincontent/maincontent.js
@@ -1,3 +1,16 @@
 Template.maincontent.helpers({
-  posts: postsData
+  enrolled: function() {
+  	if (Participants.find().count() === 0) {
+  		return false;
+  	}
+  	else{
+	    return true;
+	}
+}
+});
+
+Template.thankyou.helpers({
+	name: function() {
+		return Participants.findOne()['firstname'];
+	}
 });
