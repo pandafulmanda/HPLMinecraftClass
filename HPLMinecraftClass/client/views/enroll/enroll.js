4dabe02bc09b869785e1b441628b6de6672c1519
pandafulmanda <pandafulmanda@gmail.com>
2014-11-30 18:02:42 -0600

    separating views

---
+++ HPLMinecraftClass/client/views/enroll/enroll.js
@@ -0,0 +1,19 @@
+Template.enroll.events({'submit form' : function(event, template) {
+  event.preventDefault();
+  var data_array = Mesosphere.Utils.getFormData(template.find('form'));
+  var data = _.object(_.pluck(data_array, 'name'), _.pluck(data_array, 'value'));
+
+  Mesosphere.enrollForm.validate(data, function(errors, formFieldsObject){
+    if(!errors){
+      //Do what we need to do here;
+      Participants.insert(data, function(err, _id){
+        if(!err){
+          Session.set('currentParticipantId', _id);
+        }
+      });//, function(err) { /* handle error */ });
+      // console.log("count is up to " + Participants.find().count())
+    }else{
+      // console.log(errors);
+    }
+  });
+}});
